8527547a0303b7b9eaee474deedf360ab0387b7f
rob.treharne <rob.treharne@gmail.com>
2019-11-18 15:48:27 +0000

    9.2 added

---
+++ liverpool_everton.R
@@ -0,0 +1,65 @@
+# 9.2 Liverpool Vs Everton
+
+# This Exercise will show you how to:
+#  + investigate the descriptive statistics of a grouped dataset
+#  + create a subsample
+#  + plot a histogram (showing frequency or density)
+#  + add a normal distribution curve to a histogram
+#  + generate boxplots for the groups in your data
+
+# Task 1: Create a new "liverpool_everton.R" file in your "week_9" directory
+
+# Task 2: Download the "fan_data.csv" dataset and copy it to your week_9 directory
+
+# Task 3: Read the data file "fan_data.csv" to a dataframe
+fanHeights <- read.csv("week_9/9_2_liverpool_everton/fan_data.csv")
+
+# Task 3: Find the mean, median, range and standard deviation of the data
+
+# There are lots of ways you can do this in R.
+# The best way (I find) is to use the "describe" function from the psych package
+# To include the psych package run the line:
+library(psych)
+
+# Then you can run the describe function on your data frame
+describe(fanHeights)
+
+# You can also use th describeBy function to get the descriptive statistics for Liverpool and Everton fans seperately!
+describeBy(fanHeights, fanHeights$Team)
+
+# Task 4: Take a subsample of your fanHeights using the line below
+
+mysample <- fanHeights[sample(nrow(fanHeights), 50), ]
+describeBy(mysample, mysample$Team)
+
+# Task 5: Plot a histogram of your data
+# Don't forget to give your plot a title and label your axes accordingly
+hist(fanHeights$Height_cm,
+     main="Histogram of fan heights from a Liverpool Vs Everton match",
+     xlab="Height (cm)")
+
+# Task 6: Re-plot your histogram to show Density instead of Frequency and add a normal distribution curve to the plot.
+
+hist(fanHeights$Height_cm, 
+     freq = F,
+     main = "Height of a sample (N=2000) of fans from Liverpool vs Everton Match",
+     xlab = "Height (cm)")
+
+
+
+lines(seq(100, 240, by=1), 
+      dnorm(seq(100, 240, by=1),
+      mean(fanHeights$Height_cm), 
+      sd(fanHeights$Height_cm)), 
+      col="purple")
+
+# Task 7: Generate a box plot for the height data, grouped by team.
+boxplot(Height_cm~Team,
+        data = fanHeights,
+        ylab = "Height (cm)",
+          main="Box plots of Everton Vs Liverpool fans.")
+
+liverpool <- fanHeights[which(fanHeights$Team == "Liverpool"),]$Height_cm
+everton <- fanHeights[which(fanHeights$Team == "Everton"),]$Height_cm
+
+t.test(liverpool, everton)
