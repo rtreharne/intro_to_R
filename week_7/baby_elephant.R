975eaafd21ab83775d037360dac708398f9cfd22
rob.treharne <rob.treharne@gmail.com>
2019-11-20 08:27:45 +0000

    update

--- week_7/baby_elephant.R
+++ week_7/baby_elephant.R
@@ -1,19 +1,35 @@
-data <- read.csv("baby_elephant.csv")
+# Week 7: Introduction to R
 
+#Task 7.3
+# Read the data to a dataframe object as follows
+data <- read.csv("week_7/baby_elephant.csv")
+
+# Task 7.4
+# Add a column called "weight_kg" to your dataframe
 data$weight_kg <- data$weight_lb*0.454
 
+# Plot a scatterplot
 plot(data$time_month, data$weight_kg, xlab="Time (months)", ylab="Weight (kg)", main="Weight of a baby elephant over 35 months from birth")
 
+# Create a linear model (i.e. straight line fit)
 lmWeight <- lm(weight_kg~time_month, data=data)
 
+# output the summary of your fit
 summary(lmWeight)
 
+# Add a trendline to your plot
 lines(data$time_month, predict(lmWeight))
 
+# An easier way of doint this is ...
+abline(lmWeight)
+
+
+
+# Add the straight line equation to your plot
 text(10, 800, "y = 28.5828x + 36.9492")
 
-extr <- data.frame(time_month=80)
 
+# Predict what the elephant's weight will be after 40 months
 predict(lmWeight, newdata=data.frame(time_month=40))
 
 
--- week_9/9_3_cancer/cancer.R
+++ week_9/9_3_cancer/cancer.R
@@ -1,17 +1,36 @@
-df = read.csv("week_9/9_3_cancer/data.csv")
 
-attach(df)
+wdbc <- read.csv("week_9/9_3_cancer/wdbc.data", header=FALSE)
 
-hist(radius_mean)
+colnames(wdbc) <- c("ID",
+                    "Diagnosis",
+                    "radius",
+                    "texture",
+                    "perimeter",
+                    "area",
+                    "smoothness",
+                    "compactness",
+                    "concavity",
+                    "concave points",
+                    "symmetry",
+                    "fractal dimension")
+
+
+attach(wdbc)
+
+hist(radius,
+     xlim = c(5, 30))
+
+shapiro.test(radius_mean)
 
 malignant <- df[which(df$diagnosis == "M"),]
 benign <- df[which(df$diagnosis == "B"),]
 
 
 
-boxplot(area_mean~diagnosis, data = df)
+boxplot(compactness~diagnosis, 
+        data = df)
 
-hist(malignant$area_mean)
+hist(wdbc$`fractal dimension`)
 
 hist(benign$area_mean)
 
@@ -19,6 +38,10 @@ t.test(malignant$area_mean, benign$area_mean)
 
 library(psych)
 describeBy(df, df$diagnosis)
+
+plot(benign$radius_mean, benign$fractal_dimension_mean, col="red",
+     xlim=c(5, 30))
+points(malignant$radius_mean, malignant$fractal_dimension_mean, col="green")
 
 write.csv(describeBy(df, df$diagnosis)[1], "malignant_summary.csv")
 
@@ -28,3 +51,5 @@ legend(2000, 0.09,
        c("Malignant", "Benign"),
        col=c("red", "green"),
        pch=1)
+
+t.test(malignant$radius_mean, benign$radius_mean, var.equal=T)
