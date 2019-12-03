b195ba69326c0b589d19b874ecd29a9bdf9864a1
rob.treharne <rob.treharne@gmail.com>
2019-12-03 08:21:19 +0000

    update

--- formative_question_data/school_scores.R
+++ formative_question_data/school_scores.R
@@ -4,24 +4,37 @@ df <- read.csv("formative_question_data/school_scores.csv")
 
 which.max(by(df$English, df$School, mean))
 
-# 2. Of the areas which have over 150 pupils in it which has
+# 2. Which area has the lowest mean score for Science
+which.min(by(df$Maths, df$Area, median))
+
+# 3. What percentage of schools have class sizes greater than 40?
+
+classSizeBySchool <- data.frame(table(df$School))
+(nrow(subset(classSizeBySchool, Freq > 45))/nrow(classSizeBySchool))*100
+
+
+# 4. Of the areas which have over 150 pupils in it which has
 # the lowest mean scores for Science
 
 classSizeByArea <- data.frame(table(df$Area)) # Create a new table frame which simply counts number of entries for each Area
-classSizeByArea["Science"] <- by(df$Science, df$Area, mean) # Add column containing mean science scores
-sample <- subset(classSizeByArea, Freq>150) # Generate a subset containing "Area" with more than 150 students in them
-which.min(sample$Science) # find Area in subset with lowest science score
+classSizeByArea["Maths"] <- by(df$Maths, df$Area, mean) # Add column containing mean maths scores
+sample <- subset(classSizeByArea, Freq>150) # Generate a subset containing "Area" with more than 150 students in them which.min(sample$Maths) # find Area in subset with lowest maths score
+which.min(sample$Maths)
 
-# What is the mean class size for schools in Liverpool?
+# 5. What is the mean class size for schools in Liverpool?
+liverpool <- subset(df, Area == "Liverpool")
+df2 <- data.frame(table(liverpool$School))
+df2 <- df2[df2$Freq !=0,] # or subset(df, Freq !=0)
+mean(df2$Freq)
 
-# 4. What is the p-value of an independent t-test used to test the
+# 7. What are the t and p values of an independent t-test used to test the
 # following null hypothesis ...
 # "There is no significant difference in the mean English scores
 # of students in class sizes of above and below 35."
 
 means <- by(df$English, df$School, mean) # Get means of English scores for each school
 school_frame <- data.frame(table(df$School)) # Create new datframe containing class sizes for each school
-school_frame["mean"] <- df2 # Add English means to class_size dataframe
+school_frame["mean"] <- means # Add English means to class_size dataframe
 names(school_frame) <- c("school", "class_size", "english_mean")
 
 group_1 <- subset(school_frame, class_size<=35)$english_mean
@@ -29,29 +42,25 @@ group_2 <- subset(school_frame, class_size>35)$english_mean
 
 t.test(group_1, group_2)
 
-# 5. What are the t and p values of an independent t-test used to determine the 
+# 6. What are the t and p values of an independent t-test used to determine the 
 # following null hypothesis ..
 # "There is no significant difference in the mean Science scores
-# between students in Hoylake and West Kirby"?
+# between students in Southport and Liverpool"?
 
-# subset data
+# 6. subset data
 southport <- subset(df, Area=="Southport")
 liverpool <- subset(df, Area=="Liverpool")
 
 t.test(southport$Science, liverpool$Science)
+
+
 byArea <- data.frame(table(df$Area))
 
 df3 <-by(df$Science, df$Area, mean)
 byArea["mean"] <- df3
 
-lt <- subset(byArea, Freq>200)
+# 8.
 
-lt$Var1
+crosby <- subset(df, Area=="Crosby")
 
-slim <- df[df$Area %in% lt$Var1,]
-
-slim <- subset(df, Area %in% lt$Var1)
-
-boxplot(slim$Science,slim$Area)
-
-slim$Science
+t.test(crosby$Maths, crosby$Science, paired=TRUE)
---
+++ formative_question_data/titanic/titanic.R
@@ -0,0 +1,42 @@
+df <- read.csv("formative_question_data/titanic/titanic_data.csv")
+
+# Of those that survived what percentage were female?
+survived_female <- subset(df, df$Survived == 1 & df$Sex == "female")
+
+nrow(survived_female)/nrow(subset(df, df$Survived == 1))
+
+# What was the mean age of second class passengers?
+
+second = subset(df, df$Pclass == 2)
+mean(second$Age)
+
+# What are the t and p-values of an independent T-test used to test the following
+# null hypothesis. And, based on the result of the test should the rejected
+# or accepted?
+# "There is no difference in the mean ages of second class passanges who did
+# and did not survive"
+t.test(subset(second, Survived == 1)$Age, subset(second, Survived = 0)$Age)
+
+# What is the ratio between the percentage of first and third class children that survived?
+# Round your percentages to the nearest whole percent
+# Children < 18
+third_surv_pc = 100*nrow(subset(df, Survived == 1 & Age <18 & Pclass == 3))/nrow(subset(df, Age < 18 & Pclass == 3))
+first_surv_pc = 100*nrow(subset(df, Survived == 1 & Age <18 & Pclass == 1))/nrow(subset(df, Age <18 & Pclass == 1))
+
+# What is the difference in the mean adult fares between first and second class?
+first_fare = subset(df, Parents.Children.Aboard != 1 & Pclass == 1)
+mean(first_fare$Fare)
+not_first_fare = subset(df, Parents.Children.Aboard !=1 & Pclass == 2)
+mean(not_first_fare$Fare)
+mean(first_fare$Fare) - mean(not_first_fare$Fare)
+
+# What is the probability that you would have survived if you had been a first male who embarked from Southampton?
+survived = nrow(subset(df, Sex=="male" & Embarked=="S" & Survived == 1))
+died = nrow(subset(df, Sex=="male" & Embarked=="S" & Survived == 0)) 
+
+survived/(survived+died)
+
+# There is no difference in the mean fares of first class passengers who did and did not survive
+
+third_class = subset(df, Pclass == 1)
+t.test(subset(third_class, Survived == 1)$Fare, subset(third_class, Survived == 0)$Fare)
